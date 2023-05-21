0560fa04fd331c6471540c7753e8f82e6a13c264
Rene-Marcel Lehner <rene.lehner@tu-dortmund.de>
2023-05-21 12:38:43 +0200

    cleaning commit

--- exercises/sheet04/main.py
+++ exercises/sheet04/main.py
@@ -1,201 +1,211 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import rc
+from calculation import iterate, calc_ana_array
+from plotting import plot_potential, plot_field
 
-# ::::::::::::::::::::::::::::::::::::::::::::
-# a)
+rc('text', usetex=True)
 
-# Parameters
-delta = 0.05
-kappa = 1e-5
-N = int(1 / delta) + 1
+# LaTeX in axis-labels
+matplotlib.rcParams.update({'font.size': 12, 'text.usetex': True})
 
-print(f"Step size Delta: {delta}")
-print(f"Error bound Kappa: {kappa}")
-print(f"Discretization N: {N}")
+def main():  
+    # ::::::::::::::::::::::::::::::::::::::::::::
+    # a)
 
-# ::::::::::::::::::::::::::::::::::::::::::::
-# Gauss-Seidel algorithm
+    # Parameters
+    delta = 0.05
+    kappa = 1e-5
+    N = int(1 / delta) + 1
 
-# Perform one iteration of the Gauss-Seidel method.
-def gauss_seidel(phi, rho):
-    # Start at 1 and end at N-1 to neglect the boundaries
-    for i in range(1, N-1):
-        for j in range(1, N-1):
-            phi[i, j] = 0.25 * (phi[i-1, j] + phi[i+1, j] + phi[i, j-1] + phi[i, j+1] + delta**2 * rho[i, j])
-
-# Iterate until convergence.
-def iterate(phi, rho):
-    while True:
-        phi_old = phi.copy()
-        gauss_seidel(phi, rho)
-        if np.max(np.abs(phi - phi_old)) < kappa:
-            break
+    print(f"Step size Delta: {delta}")
+    print(f"Error bound Kappa: {kappa}")
+    print(f"Discretization N: {N}")
         
-# ::::::::::::::::::::::::::::::::::::::::::::
-# b)
-        
-# Initialize arrays
-phi = np.ones((N, N))  # potential
-rho = np.zeros((N, N))  # charge density
+    # ::::::::::::::::::::::::::::::::::::::::::::
+    # b)
+            
+    # Initialize arrays
+    phi = np.ones((N, N))  # potential
+    rho = np.zeros((N, N))  # charge density
 
-# Boundary conditions
-phi[0, :] = 0
-phi[-1, :] = 0
-phi[:, 0] = 0
-phi[:, -1] = 0
+    # Boundary conditions
+    phi[0, :] = 0
+    phi[-1, :] = 0
+    phi[:, 0] = 0
+    phi[:, -1] = 0
 
-# Perform the Gauss-Seidel method
-iterate(phi, rho)
+    # Perform the Gauss-Seidel method
+    iterate(phi, rho, kappa, N, delta)
 
-# Plot the potential
-plt.imshow(phi, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Potential')
-plt.show()
+    # Plotting
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # Create a mosaic subplot
+    fig, axs = plt.subplot_mosaic("AB", figsize=(10, 6))
 
-# Calculate electric field
-E = np.gradient(-phi)
+    # Plot the potential
+    im = plot_potential(phi, axs['A'], fig, "Potential")
 
-# Plot the electric field
-plt.quiver(E[1], E[0])
-plt.show()
+    # Calculate electric field
+    E = np.gradient(-phi)
 
-# ::::::::::::::::::::::::::::::::::::::::::::
-# c)
+    # Plot the electric field
+    plot_field(E, axs['B'], fig, im, N, "Electric Field")
 
-# Reset potential
-phi = np.zeros((N, N)) 
+    fig.suptitle("Uniform, positive potential with uniform boundary conditions\n" + r"\textbf{Gauss-Seidel algorithm}")
+    fig.tight_layout()
+    fig.savefig("b.pdf")
+    plt.show()
 
-# Boundary conditions
-phi[0, :] = 1
-phi[-1, :] = 0
-phi[:, 0] = 0
-phi[:, -1] = 0
+    # ::::::::::::::::::::::::::::::::::::::::::::
+    # c)
 
-# Iterate with Gauss-Seidel method
-iterate(phi, rho)
+    # Reset potential
+    phi = np.zeros((N, N)) 
 
-# Plot the potential
-plt.imshow(phi, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Potential')
-plt.show()
+    # Boundary conditions
+    phi[0, :] = 1
+    phi[-1, :] = 0
+    phi[:, 0] = 0
+    phi[:, -1] = 0
 
-# Calculate electric field
-E = np.gradient(-phi)
+    # Iterate with Gauss-Seidel method
+    iterate(phi, rho, kappa, N, delta)
 
-# Plot the electric field
-plt.quiver(E[1], -E[0])
-# Invert y-axis
-# Because quiver interprets the starting point at the bottom left corner, while imshow starts at the top left corner
-plt.gca().invert_yaxis()
-plt.show()
+    # Plotting
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # Create a mosaic subplot
+    fig, axs = plt.subplot_mosaic("AB", figsize=(10, 6))
 
-# Comparation of numerical with the analytical result
-# Analytical function
-def analytical_phi(x, y):
-    steps = 10000
-    result = 0
-    eps = 1e-10  # Define a threshold for the minimum term size
-    for n in range(1, steps + 1):
-        npi = n * np.pi
-        term = 2 * (1 - np.cos(npi)) / (npi * np.sinh(npi)) * np.sin(npi * x) * np.sinh(npi * y)
-        if abs(term) < eps:  # If the term is smaller than the threshold, break the loop
-            break
-        result += term
-    return result
+    # Plot the potential
+    plot_potential(phi, axs['A'], fig, "Potential - Numerical")
 
+    # Calculate electric field
+    E = np.gradient(-phi)
 
-# Calculate the complete grid
-def calc_ana_array(N):
-    result = np.zeros((N, N))
-    for i in range(N):
-        for k in range(N):
-            result[N - k - 1, i] = analytical_phi(i * delta, k * delta)
-    return result  
+    # Plot the electric field
+    plot_field(E, axs['B'], fig, im, N, "Electric Field - Numerical")
 
-ana = calc_ana_array(N)
+    fig.suptitle("Upper boundary potential\n" + r"\textbf{Gauss-Seidel algorithm}")
+    fig.tight_layout()
+    fig.savefig("c_numerical.pdf")
 
-# Plot the analytical solution
-plt.imshow(ana, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Analytical solution')
-plt.show()
+    plt.show()
+    # :::::::::::::::::::::::::::::::::::::::::::
+    
+    # Calculate the analytical solution
+    ana = calc_ana_array(N, delta)
 
-# Calculate electric field
-E = np.gradient(-ana)
+    # Plotting
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # Plot the analytical solution
+    fig, axs = plt.subplot_mosaic("AB", figsize=(10, 6))
 
-# Plot the electric field
-plt.quiver(E[1], -E[0])
-# Invert y-axis
-# Because quiver interprets the starting point at the bottom left corner, while imshow starts at the top left corner
-plt.gca().invert_yaxis()
-plt.show()
+    plot_potential(ana, axs['A'], fig, "Potential - Analytical")
 
-# Plot the difference
-plt.imshow(ana - phi, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Analytical - Numerical solution')
-plt.show()
+    # Calculate electric field
+    E = np.gradient(-ana)
 
+    # Plot the electric field
+    plot_field(E, axs['B'], fig, im, N, "Electric Field - Analytical")
 
-# Reset potential and charge density
-phi = np.zeros((N, N)) 
-rho = np.zeros((N, N)) 
+    fig.suptitle("Analytical Solution to the upper boundary potential")
+    fig.tight_layout()
+    fig.savefig("c_analytical.pdf")
 
-# Place charge at the center
-rho[N//2, N//2] = 1
+    plt.show()
+    # :::::::::::::::::::::::::::::::::::::::::::
+    
+    # Plot the difference in a single plot
+    plt.imshow(ana - phi, cmap='viridis', extent=(0, 1, 0, 1))
+    plt.colorbar(label='Analytical - Numerical solution')
+    plt.title("Difference: Analytical - Numerical\nUpper boundary potential")
+    plt.savefig("c_difference.pdf")
+    plt.show()
 
-# Boundary conditions
-phi[0, :] = 0
-phi[-1, :] = 0
-phi[:, 0] = 0
-phi[:, -1] = 0
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # d)
 
-# Iterate with Gauss-Seidel method
-iterate(phi, rho)
+    # Reset potential and charge density
+    phi = np.zeros((N, N)) 
+    rho = np.zeros((N, N)) 
 
-# Calculate electric field
-E = np.gradient(-phi)
+    # Place charge at the center
+    rho[N//2, N//2] = 1
 
-# Plot the potential
-plt.imshow(phi, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Potential')
-#plt.show()
+    # Boundary conditions
+    phi[0, :] = 0
+    phi[-1, :] = 0
+    phi[:, 0] = 0
+    phi[:, -1] = 0
 
-# Plot the electric field
-plt.quiver(E[1], E[0])
-plt.show()
+    # Iterate with Gauss-Seidel method
+    iterate(phi, rho, kappa, N, delta)
 
-# Reset potential and charge density
-phi = np.zeros((N, N)) 
-rho = np.zeros((N, N)) 
+    # Plotting
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # Create a mosaic subplot
+    fig, axs = plt.subplot_mosaic("AB", figsize=(10, 6))
 
-# Place charges
-rho[int(0.25/delta), int(0.25/delta)] = 1
-rho[int(0.75/delta), int(0.75/delta)] = 1
-rho[int(0.25/delta), int(0.75/delta)] = -1
-rho[int(0.75/delta), int(0.25/delta)] = -1
+    # Plot the potential
+    plot_potential(phi, axs['A'], fig, "Potential")
 
-# Boundary conditions
-phi[0, :] = 0
-phi[-1, :] = 0
-phi[:, 0] = 0
-phi[:, -1] = 0
+    # Calculate electric field
+    E = np.gradient(-phi)
 
-# Iterate with Gauss-Seidel method
-iterate(phi, rho)
+    # Plot the electric field
+    plot_field(E, axs['B'], fig, im, N, "Electric Field")
 
-# Calculate electric field
-E = np.gradient(-phi)
+    fig.suptitle("Single Charge, center\n" + r"\textbf{Gauss-Seidel algorithm}")
+    fig.tight_layout()
+    fig.savefig("d_single.pdf")
 
-# Plot the potential
-plt.imshow(phi, cmap='viridis', extent=(0, 1, 0, 1))
-plt.colorbar(label='Potential')
-plt.show()
+    plt.show()
+    # :::::::::::::::::::::::::::::::::::::::::::
+    
+    # e)
+    
+    # Reset potential and charge density
+    phi = np.zeros((N, N)) 
+    rho = np.zeros((N, N)) 
 
-# Calculate electric field
-E = np.gradient(-phi)
+    # Place charges
+    rho[int(0.25/delta), int(0.25/delta)] = 1
+    rho[int(0.75/delta), int(0.75/delta)] = 1
+    rho[int(0.25/delta), int(0.75/delta)] = -1
+    rho[int(0.75/delta), int(0.25/delta)] = -1
 
-# Plot the electric field
-plt.quiver(E[1], -E[0])
-# Invert y-axis
-# Because quiver interprets the starting point at the bottom left corner, while imshow starts at the top left corner
-plt.gca().invert_yaxis()
-plt.show()
+    # Boundary conditions
+    phi[0, :] = 0
+    phi[-1, :] = 0
+    phi[:, 0] = 0
+    phi[:, -1] = 0
+
+    # Iterate with Gauss-Seidel method
+    iterate(phi, rho, kappa, N, delta)
+
+    # Plotting
+    # :::::::::::::::::::::::::::::::::::::::::::
+    # Create a mosaic subplot
+    fig, axs = plt.subplot_mosaic("AB", figsize=(10, 6))
+
+    # Plot the potential
+    plot_potential(phi, axs['A'], fig, "Potential - Four Charges")
+
+    # Calculate electric field
+    E = np.gradient(-phi)
+
+    # Plot the electric field
+    plot_field(E, axs['B'], fig, im, N, "Electric Field - Four Charges")
+
+    fig.suptitle("Four Charges\n" + r"\textbf{Gauss-Seidel algorithm}")
+    fig.tight_layout()
+    fig.savefig("e_four_charges.pdf")
+
+    plt.show()
+    # :::::::::::::::::::::::::::::::::::::::::::
+    
+    
+if __name__ == "__main__":
+    main()
